5f5ff65c5657b4da4be88ebd413214892efab2e7
James <mendozajames02@hotmail.com>
2021-06-05 15:33:44 -0500

    Fix a problem with Header animation

--- public/headerAnimation.js
+++ public/headerAnimation.js
@@ -1,10 +1,17 @@
 const header = document.querySelector('.header')
+let isHeaderActive = false
 
 window.addEventListener('scroll', () => {
-    let currentScroll = window.pageYOffset
-    if(currentScroll > 60){
-        header.classList.add('header-active')
-    } else if(currentScroll < 60){
-        header.classList.remove('header-active')
-    }
+  let currentScroll = window.pageYOffset
+  if(currentScroll > 60){
+    header.classList.add('header-active')
+  } else if(currentScroll < 60){
+    header.classList.remove('header-active')
+  }
+
+  isHeaderActive = header.classList.contains('header-active')
+  isHeaderMenuActive()
 })
+
+
+
--- public/menuMobile.js
+++ public/menuMobile.js
@@ -1,9 +1,12 @@
 const menuMobile = document.getElementById('menu-icon')
 const menu = document.getElementById('menu')
 const links = document.querySelectorAll('#menu a')
+let isMenuActive = false
 
 menuMobile.addEventListener('click', () => {
   menu.classList.toggle('menu-mobile-active')
+  isMenuActive = menu.classList.contains('menu-mobile-active')
+  isHeaderMenuActive()
 })
 
 links.forEach(link => {
@@ -11,6 +14,18 @@ links.forEach(link => {
     menu.classList.toggle('menu-mobile-active')
   })
 })
+
+function isHeaderMenuActive() {
+  if(isHeaderActive && isMenuActive) {
+    header.classList.add('header-active')
+  }
+  if (!isHeaderActive && isMenuActive){
+    header.classList.add('header-active')
+  }
+  if(!isMenuActive && !isHeaderActive) {
+    header.classList.remove('header-active')
+  }
+}
 
 
 window.onresize = () => window.innerWidth >= 1024 ? menu.classList.remove('menu-mobile-active') : null
