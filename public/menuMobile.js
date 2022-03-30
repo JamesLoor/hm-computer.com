0c37540bebdf550b9ca446a4e6359be5ce8e6ab9
unknown <derianpaez01@hotmail.com>
2022-03-30 18:21:08 -0500

    add header with responsive and starting hero

--- public/menuMobile.js
+++ public/menuMobile.js
@@ -1,31 +1,31 @@
-const menuMobile = document.getElementById('menu-icon')
-const menu = document.getElementById('menu')
-const links = document.querySelectorAll('#menu a')
-let isMenuActive = false
+// const menuMobile = document.getElementById('menu-icon')
+// const menu = document.getElementById('menu')
+// const links = document.querySelectorAll('#menu a')
+// let isMenuActive = false
 
-menuMobile.addEventListener('click', () => {
-  menu.classList.toggle('menu-mobile-active')
-  isMenuActive = menu.classList.contains('menu-mobile-active')
-  isHeaderMenuActive()
-})
+// menuMobile.addEventListener('click', () => {
+//   menu.classList.toggle('menu-mobile-active')
+//   isMenuActive = menu.classList.contains('menu-mobile-active')
+//   isHeaderMenuActive()
+// })
 
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    menu.classList.toggle('menu-mobile-active')
-  })
-})
+// links.forEach(link => {
+//   link.addEventListener('click', () => {
+//     menu.classList.toggle('menu-mobile-active')
+//   })
+// })
 
-function isHeaderMenuActive() {
-  if(isHeaderActive && isMenuActive) {
-    header.classList.add('header-active')
-  }
-  if (!isHeaderActive && isMenuActive){
-    header.classList.add('header-active')
-  }
-  if(!isMenuActive && !isHeaderActive) {
-    header.classList.remove('header-active')
-  }
-}
+// function isHeaderMenuActive() {
+//   if(isHeaderActive && isMenuActive) {
+//     header.classList.add('header-active')
+//   }
+//   if (!isHeaderActive && isMenuActive){
+//     header.classList.add('header-active')
+//   }
+//   if(!isMenuActive && !isHeaderActive) {
+//     header.classList.remove('header-active')
+//   }
+// }
 
 
-window.onresize = () => window.innerWidth >= 1024 ? menu.classList.remove('menu-mobile-active') : null
+// window.onresize = () => window.innerWidth >= 1024 ? menu.classList.remove('menu-mobile-active') : null
